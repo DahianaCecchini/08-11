a5c9f00a39a4e9e7c96b4823f2572ab5ad9873c5
Dahiana <cecchini.dahiana@hotmail.com>
2022-01-16 18:26:40 -0300

    actualizado

--- proyecto-react/src/PaginaReporte/ReporteDiarioEHistorial.jsx
+++ proyecto-react/src/PaginaReporte/ReporteDiarioEHistorial.jsx
@@ -1,15 +1,19 @@
 import React, { useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import '../IndexPrincipal/CssIndex/iPadCss.css';
+import '../IndexPrincipal/CssIndex/pcCss.css';
 
 const Reporte = () => {
 
-  const [startDate, setStartDate] = useState(new Date());
+const [startDate, setStartDate] = useState(new Date());
   return (
     <div id="Fecha">
     <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
+    <button id="Buscar">BUSCAR</button>
     </div>
   );
 };
+
 export default Reporte;
 
